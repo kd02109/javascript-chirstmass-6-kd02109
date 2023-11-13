5defd64dfee3ade6a9e7ea839e3bf0551a66df97
JUNSEOK <kd02109@gmail.com>
2023-11-13 22:50:41 +0900

    Refactor: ReservationDateValidation 주석 추가

--- src/validation/ReservationDateValidation.js
+++ src/validation/ReservationDateValidation.js
@@ -3,9 +3,18 @@ import { ERROR_MESSAGE } from '../constants/message.js';
 import { START_DAY, END_DAY } from '../constants/constant.js';
 
 const ReservationDateValidation = Object.freeze({
+  /**
+   * 숫자만 입력했는지 확인
+   * @param {number} date
+   */
   validateNumber(date) {
     if (Number.isNaN(date)) CustomError.inputView(ERROR_MESSAGE.date);
   },
+
+  /**
+   * 1~31 사이의 수만 입력했는지 확인
+   * @param {number} date
+   */
   validateUnderAndOver(date) {
     if (date < START_DAY || date > END_DAY) CustomError.inputView(ERROR_MESSAGE.date);
   },
